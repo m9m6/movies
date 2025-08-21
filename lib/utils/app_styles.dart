771e74a792ebc537f3650bdc6136459ae1422e7b
Mariam Badr <mariamalaabdr@gmail.com>
2025-08-21 21:21:40 +0300

    Splash Screen + all onboarding section (ui and navigation)

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -12,8 +12,12 @@ class AppStyles {
       fontSize: 18, fontWeight: FontWeight.w400, color: AppColors.whiteColor);
   static TextStyle regular20White = GoogleFonts.roboto(
       fontSize: 20, fontWeight: FontWeight.w400, color: AppColors.whiteColor);
-  static TextStyle regular24White = GoogleFonts.roboto(
+  static TextStyle inter20White = GoogleFonts.inter(
+      fontSize: 20, fontWeight: FontWeight.w400, color: AppColors.whiteColor);
+  static TextStyle inter24White = GoogleFonts.inter(
       fontSize: 24, fontWeight: FontWeight.w700, color: AppColors.whiteColor);
+  static TextStyle large36White = GoogleFonts.inter(
+      fontSize: 36, fontWeight: FontWeight.w500, color: AppColors.whiteColor,);
 
   // ========= Black Colors =========
   static TextStyle regular12Black = GoogleFonts.roboto(
--- lib/utils/assets_manager.dart
+++ lib/utils/assets_manager.dart
@@ -1,2 +1,10 @@
 class AssetsManager {
+
+  static const String moviesPostersGroup1 = 'assets/images/movies_posters_group_1.png';
+  static const String moviesPostersGroup2 = 'assets/images/movies_posters_group_2.png';
+  static const String moviesPostersGroup3 = 'assets/images/movies_posters_group_3.png';
+  static const String moviesPostersGroup4 = 'assets/images/movies_posters_group_4.png';
+  static const String moviesPostersGroup5 = 'assets/images/movies_posters_group_5.png';
+  static const String moviesPostersGroup6 = 'assets/images/movies_posters_group_6.png';
+
 }
