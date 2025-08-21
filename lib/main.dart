5296db9d8666bb34b96ec084da4c46f951ca659f
youssef rabea <youssef112200@outlook.com>
2025-08-22 01:05:25 +0300

    + Added login screen + Added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movies/ui/auth/login/login_screen.dart';
+import 'package:movies/ui/home_screens/home_screen.dart';
 import 'package:movies/ui/update_profile/cubit/profile_cubit.dart';
 import 'package:movies/ui/update_profile/profile_screen.dart';
 import 'package:movies/ui/onboarding/onboarding_start.dart';
@@ -19,6 +21,8 @@ class MyApp extends StatelessWidget {
         AppRoutes.preBoarding:(context)=>PreOnboardingScreen(),
         AppRoutes.onBoarding:(context)=>OnboardingStart(),
         AppRoutes.ProfileScreen:(context)=> ProfileScreen(),
+        AppRoutes.loginScreen:(context)=>LoginScreen(),
+        AppRoutes.homeScreen:(context)=>HomeScreen(),
       },
       theme: AppThemes.darkTheme,
       themeMode: ThemeMode.dark,
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -3,4 +3,6 @@ class AppRoutes{
   static const String RouteName = 'put the name here ';
   static const String onBoarding = 'onboarding_start';
   static const String preBoarding = 'pre_onboarding';
+  static const String loginScreen = 'login_screen';
+  static const String homeScreen = 'home_screen';
 }
