92185dfcb39f03e723e9077ce85c242f07496daf
sohilasalah <sohilasalah185@gmail.com>
2025-08-21 22:27:06 +0300

    creating profile screen + update profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:movies/ui/update_profile/profile_screen.dart';
 import 'package:movies/utils/app_routes.dart';
 import 'package:movies/utils/app_themes.dart';
-
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // initialRoute: AppRoutes.RouteName,
-      routes: {
+      initialRoute: AppRoutes.RouteName,
+      //routes: {
         // AppRoutes.RouteName:(context)=>the Screen here(),
-      },
+      //},
       theme: AppThemes.darkTheme,
       themeMode: ThemeMode.dark,
     );
---
+++ lib/ui/update_profile/widgets/edit_account.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/ui/update_profile/widgets/profile_avatar.dart';
+import '../../../utils/app_colors.dart';
+import '../../../utils/app_styles.dart';
+import '../../custom_widgets/custom_elevatedButton.dart';
+import '../../custom_widgets/custom_textFormFeild.dart';
+import '../cubit/profile_cubit.dart';
+import 'avatar_sheet.dart';
+class EditAccount extends StatefulWidget {
+  const EditAccount({super.key});
+  @override
+  State<EditAccount> createState() => _EditAccountState();
+}
+class _EditAccountState extends State<EditAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('pick Avatar', style: AppStyles.bold24Yellow),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BlocBuilder<ProfileCubit, ProfileState>(
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Column(
+                spacing: 10,
+                children: [
+                  ProfileAvatar(imageUrl: 'assets/gamer (1) (1).png'),
+                  CustomTextFormField(
+                    controller: context
+                        .read<ProfileCubit>()
+                        .nameController, prefixIcon: Icon(Icons.person), hintText: 'John Safwat',
+                  ),
+                  CustomTextFormField(
+                    hintText: '01200000000',
+                    prefixIcon: Icon(Icons.call),
+                    controller: context
+                        .read<ProfileCubit>()
+                        .phoneController,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Reset Password',
+                        style: AppStyles.regular16White,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(MediaQuery
+            .sizeOf(context)
+            .width * 0.05),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 10,
+          children: [
+            CustomElevatedButton(
+              textColor: AppColors.whiteColor,
+              text: 'Delete Account',
+              backgroundColor: AppColors.redColor,
+              onPressed: () {},
+            ),
+            CustomElevatedButton(
+              textColor: AppColors.darkBlackColor,
+              text: 'Update Data',
+              backgroundColor: AppColors.yellowColor,
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  scrollControlDisabledMaxHeightRatio: 0.45,
+                  builder: (context) {
+                    return AvatarSheet();
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
